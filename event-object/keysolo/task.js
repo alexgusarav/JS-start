f97a0a3d52f7b4038020d751a241a83cea6e0b90
alexgusarav <159787148+alexgusarav@users.noreply.github.com>
2025-01-08 14:39:10 +0300

    event-object
    event-object

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,14 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+      document.addEventListener("keydown", event => {
+        const currentSymbol = this.currentSymbol.innerHTML;
+          if (currentSymbol.toLocaleLowerCase() === event.key.toLocaleLowerCase()) {
+            this.success();
+          } else {
+            this.fail();
+          }
+        });
   }
 
   success() {
